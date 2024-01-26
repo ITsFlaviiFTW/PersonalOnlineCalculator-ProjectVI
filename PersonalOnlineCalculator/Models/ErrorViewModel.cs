16ae5d0fa52af13e789866fc5e1ecc89c8d4dcad
Flavius <flaviusporumbiel@outlook.com>
2024-01-26 09:15:24 -0500

    Add project files.

---
+++ PersonalOnlineCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalOnlineCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
