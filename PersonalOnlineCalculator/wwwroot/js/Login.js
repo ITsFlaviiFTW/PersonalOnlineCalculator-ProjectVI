ed206769a12f7e93bfc8c5213fcec9c096e956f1
Patryk Z <patryk.zbikowski01@gmil.com>
2024-01-28 16:58:32 -0500

    Fixed major issues with overlapping page styles, and page redirects were made

--- PersonalOnlineCalculator/wwwroot/js/Login.js
+++ PersonalOnlineCalculator/wwwroot/js/Login.js
@@ -1,7 +1,7 @@
 let signup = document.querySelector(".signup");
 let login = document.querySelector(".login");
-let slider = document.querySelector(".slider");
-let formSection = document.querySelector(".form-section");
+let slider = document.querySelector(".loginSlider");
+let formSection = document.querySelector(".login-form-section");
  
 signup.addEventListener("click", () => {
     slider.classList.add("moveslider");
